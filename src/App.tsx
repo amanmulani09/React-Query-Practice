3bd8f2e3031d90af9c354551477f72c599a6516b
amanmulani09 <mulaniaman0504@gmail.com>
2023-08-30 11:01:58 +0530

    learned about query and mutation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,36 @@
+import {useQuery,useMutation} from '@tanstack/react-query'
+
+const POSTS =[
+  {
+    id:1,
+    title:'post 1'
+  },
+  {
+    id:2,
+    title:'post 2'
+  }
+];
 
 const App = ()=>{
-  return <h1> App </h1>
+  const postQuery = useQuery({
+    queryKey:["posts"],
+    queryFn: ()=> wait(1000).then(()=> [...POSTS])
+  })
+
+if(postQuery.isLoading) return <h1>...Loading</h1>
+if(postQuery.isError) return <pre>{JSON.stringify(postQuery.error)}</pre>
+
+  return(
+    <>
+    {postQuery.data.map((data)=>{
+      return <p>{data.title}</p>
+    })}
+    </>
+  )
+}
+
+function wait(duration:number){
+return new Promise(resolve => setTimeout(resolve, duration));
 }
 
 export default App;
